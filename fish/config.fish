412d2e4cae404aa509b64cf18683db30707c3458
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2023-01-29 11:49:16 +0530

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 starship init fish | source
 colorscript random
-export OPENAI_API_KEY=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
+
 export IP=172.105.35.245
 set fish_greeting
 
