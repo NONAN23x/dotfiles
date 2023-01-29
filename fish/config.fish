d17b3b9c95ca35eb32c8ab654b8057a2c0b2be2a
Nomaan Jamaluddin <nonan23x@notebook.localdomain>
2023-01-29 11:47:14 +0530

    Added few configs to the repo

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+colorscript random
+export OPENAI_API_KEY=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
+export IP=172.105.35.245
+set fish_greeting
+
+fish_add_path /home/nonan23x/.spicetify
