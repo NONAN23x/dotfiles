a4e4dfa188b9a2a405543cd1d5ca861ccc661c5d
Nomaan Jamaluddin <nomaanjamaluddin@gmail.com>
2024-04-04 12:27:58 +0530

    New Age

---
+++ config/hypr/switchwall.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+
+# ██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+# ██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+# ██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝
+# ██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+# ╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║
+#  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+# setting a wallpaper
+if [ "$1" == "--noswitch" ]; then
+    imgpath=$(swww query | awk -F 'image: ' '{print $2}')
+else
+    cd "$HOME/Pictures/Wallpapers"
+    imgpath=$(yad --width 1200 --height 800 --file --title='Choose wallpaper')
+    screensizey=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2 | head -1)
+    # cursorposx=$(hyprctl cursorpos -j | gojq '.x' 2>/dev/null) || cursorposx=960
+    # cursorposy=$(hyprctl cursorpos -j | gojq '.y' 2>/dev/null) || cursorposy=540
+    # cursorposy_inverted=$(( screensizey - cursorposy ))
+
+    if [ "$imgpath" == '' ]; then
+        echo 'Aborted'
+        exit 0
+    fi
+
+    swww img "$imgpath" --transition-step 100 --transition-fps 60 \
+    --transition-type any --transition-duration 2
+    # ██████╗ ██╗   ██╗██╗    ██╗ █████╗ ██╗     
+    # ██╔══██╗╚██╗ ██╔╝██║    ██║██╔══██╗██║     
+    # ██████╔╝ ╚████╔╝ ██║ █╗ ██║███████║██║     
+    # ██╔═══╝   ╚██╔╝  ██║███╗██║██╔══██║██║     
+    # ██║        ██║   ╚███╔███╔╝██║  ██║███████╗
+    # ╚═╝        ╚═╝    ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝
+    # pywal
+    wal -q -e -i ${imgpath} 
+    rm $HOME/.wallpapers/currentWall
+    ln -s ${imgpath} $HOME/.wallpapers/currentWall
+    pywal-discord
+    
+    
+    # ███╗   ███╗ ██████╗ ███╗   ██╗███████╗████████╗
+    # ████╗ ████║██╔═══██╗████╗  ██║██╔════╝╚══██╔══╝
+    # ██╔████╔██║██║   ██║██╔██╗ ██║█████╗     ██║   
+    # ██║╚██╔╝██║██║   ██║██║╚██╗██║██╔══╝     ██║   
+    # ██║ ╚═╝ ██║╚██████╔╝██║ ╚████║███████╗   ██║   
+    # ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝
+    gradience-cli monet -n "pywal" -p ${imgpath} --theme dark
+    gradience-cli apply -n "pywal"
+    gradience-cli flatpak-overrides -e both
+
+fi
+
+
