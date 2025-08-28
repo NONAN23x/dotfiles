a9dbcb91f8b187194b19079e66678157024783a8
Nomaan Jamaluddin <nomaanjamaluddin@gmail.com>
2025-08-28 07:39:13 +0530

    Updates

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -1,7 +1,7 @@
 local options = {
 
   base46 = {
-    theme = "catppuccin", -- default theme
+    theme = "rosepine", -- default theme
     hl_add = {},
     hl_override = {},
     integrations = {},
